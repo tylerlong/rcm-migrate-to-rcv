d8a52187373446f6f35f4f292f715a35c825b9f0
Tyler Long <tyler4long@gmail.com>
2020-08-31 11:34:43 -0700

    msal

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log('Hello world');
+import * as Msal from 'msal';
+
+const msalConfig = {
+  auth: {
+    clientId: process.env.MSAL_CLIENT_ID!,
+  },
+};
+
+const msalInstance = new Msal.UserAgentApplication(msalConfig);
+
+console.log(process.env.MSAL_CLIENT_ID);
