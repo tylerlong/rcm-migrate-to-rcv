6c1b668736094a84ebd97da7214e76bd018694a4
Tyler Long <tyler4long@gmail.com>
2020-08-31 17:09:25 -0700

    Step #2

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {Component} from 'react-subx';
-import {Button} from 'antd';
+import {Button, Divider} from 'antd';
 
 import {StoreType} from './store';
 
@@ -13,9 +13,19 @@ class App extends Component<PropsStore> {
     return (
       <>
         <h1>RCM migrate to RCV</h1>
+
+        <Divider />
+
         <h2>Step #1</h2>
         <Button onClick={() => store.login()} type="primary">
           Authorize this app to access your Outlook Calendar
+        </Button>
+
+        <Divider />
+
+        <h2>Step #2</h2>
+        <Button onClick={() => store.migrate()} type="primary">
+          Migrate your RCM meetings to RCV meetings
         </Button>
       </>
     );
--- src/store.ts
+++ src/store.ts
@@ -1,15 +1,16 @@
 import SubX from 'subx';
 import * as MSAL from 'msal';
 import * as graph from '@microsoft/microsoft-graph-client';
-// eslint-disable-next-line node/no-unpublished-import
-import waitFor from 'wait-for-async';
 
 export type StoreType = {
   ready: boolean;
   init: Function;
   load: Function;
   login: Function;
+  migrate: Function;
 };
+
+let client: graph.Client;
 
 const userAgentApplication = new MSAL.UserAgentApplication({
   auth: {
@@ -27,30 +28,21 @@ const store = SubX.proxy<StoreType>({
   init() {},
   load() {},
   async login() {
-    const loginResponse = await userAgentApplication.loginPopup({
+    await userAgentApplication.loginPopup({
       scopes: ['Calendars.ReadWrite'],
     });
-    console.log(loginResponse);
     const tokenResponse = await userAgentApplication.acquireTokenSilent({
       scopes: ['Calendars.ReadWrite'],
     });
-    console.log(tokenResponse);
-    const client = graph.Client.init({
+    client = graph.Client.init({
       authProvider: done => {
         done(null, tokenResponse.accessToken);
       },
     });
-
-    await waitFor({interval: 3000});
-
+  },
+  async migrate() {
     const events = await client.api('/me/calendar/events').get();
     console.log(events);
-    // const r = await axios.get('/me/calendar/events', {
-    //   headers: {
-    //     Authorization: `Bearer ${tokenResponse.accessToken}`,
-    //   },
-    // });
-    // console.log(r.data);
   },
 });
 
