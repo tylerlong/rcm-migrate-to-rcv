ff03229f53f9279d64157e8c869ff7a02380d9d8
Tyler Long <tyler4long@gmail.com>
2020-09-04 17:24:04 -0700

    List users and get their calendar

--- src/google/index.ts
+++ src/google/index.ts
@@ -1,23 +1,35 @@
 import {google} from 'googleapis';
 import path from 'path';
 
-const scopes = ['https://www.googleapis.com/auth/calendar'];
+const scopes = [
+  'https://www.googleapis.com/auth/admin.directory.user',
+  'https://www.googleapis.com/auth/calendar',
+];
 
-const googleAuth = new google.auth.GoogleAuth({
-  keyFile: path.join(__dirname, 'service-account.json'),
-  scopes,
-  clientOptions: {
-    // subject: 'peterlong@chuntaoliu.com',
-    subject: 'tylerliu@chuntaoliu.com',
-  },
-});
-
-const googleCalendar = google.calendar({
-  version: 'v3',
-  auth: googleAuth,
-});
+const getGoogleAuth = (subject = 'tylerliu@chuntaoliu.com') => {
+  return new google.auth.GoogleAuth({
+    keyFile: path.join(__dirname, 'service-account.json'),
+    scopes,
+    clientOptions: {subject},
+  });
+};
 
 (async () => {
-  const r = await googleCalendar.calendarList.list();
-  console.log(JSON.stringify(r.data, null, 2));
+  const r1 = await google
+    .admin({
+      version: 'directory_v1',
+      auth: getGoogleAuth('tylerliu@chuntaoliu.com'),
+    })
+    .users.list({domain: 'chuntaoliu.com'});
+  console.log(JSON.stringify(r1.data, null, 2));
+
+  for (const user of r1.data.users ?? []) {
+    const r2 = await google
+      .calendar({
+        version: 'v3',
+        auth: getGoogleAuth(user.primaryEmail!),
+      })
+      .calendarList.list();
+    console.log(JSON.stringify(r2.data, null, 2));
+  }
 })();
