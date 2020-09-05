1bff6394041d5146a86f5abae29da5bdf80f3e22
Tyler Long <tyler4long@gmail.com>
2020-09-04 17:49:00 -0700

    Find match Google events

--- src/google/index.ts
+++ src/google/index.ts
@@ -6,6 +6,8 @@ const scopes = [
   'https://www.googleapis.com/auth/calendar',
 ];
 
+const rcmMeetingRegex = /https:\/\/meetings\.ringcentral\.com\/j\/\d+/;
+
 const getGoogleAuth = (subject = 'tylerliu@chuntaoliu.com') => {
   return new google.auth.GoogleAuth({
     keyFile: path.join(__dirname, 'service-account.json'),
@@ -24,12 +26,22 @@ const getGoogleAuth = (subject = 'tylerliu@chuntaoliu.com') => {
   console.log(JSON.stringify(r1.data, null, 2));
 
   for (const user of r1.data.users ?? []) {
-    const r2 = await google
-      .calendar({
-        version: 'v3',
-        auth: getGoogleAuth(user.primaryEmail!),
-      })
-      .calendarList.list();
+    const calendar = google.calendar({
+      version: 'v3',
+      auth: getGoogleAuth(user.primaryEmail!),
+    });
+    const r2 = await calendar.events.list({calendarId: 'primary'});
     console.log(JSON.stringify(r2.data, null, 2));
+    const events = r2.data.items?.filter(i => i.organizer?.self === true);
+    for (const event of events ?? []) {
+      let match = event.location?.match(rcmMeetingRegex);
+      if (match === null) {
+        match = event.description?.match(rcmMeetingRegex);
+      }
+      if (match !== null) {
+        console.log('Found a match:', JSON.stringify(event, null, 2));
+        // todo: update this event
+      }
+    }
   }
 })();
